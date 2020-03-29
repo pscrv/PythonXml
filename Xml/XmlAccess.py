e1ba245fcb9caaeaa77e9fcbfd483056f1fd89e9
ps <scrv.pscrv@googlemail.com>
2020-03-29 17:23:37 +0200

    _searchResult

--- Xml/XmlAccess.py
+++ Xml/XmlAccess.py
@@ -1,26 +1,66 @@
-from os import  PathLike
+from os import PathLike
+from abc import ABC, abstractmethod, abstractproperty
 
+from Xml.Decision import Decision
 from Xml.XmlHandler import FileReader
+
+
+
+class _searchResult( ABC ) :
+
+        @abstractmethod
+        def Add ( self, decision: Decision ) : ...
+
+        @property
+        @abstractmethod
+        def Result ( self ) : ...
+
+
+class _countResult( _searchResult ) :
+
+    def __init__ ( self ) :
+        self._result = 0
+
+    def Add ( self, decision ) :
+        self._result += 1
+
+    @property
+    def Result ( self ) :
+        return self._result
+
+
+class _listResult( _searchResult ) :
+
+        def __init__ ( self ) :
+            self._result = [ ]
+
+        def Add ( self, decision: Decision ) :
+            self._result.append( decision )
+
+        @property
+        def Result ( self ) :
+            return self._result
+
 
 
 class Accessor :
 
-    def __init__ ( self, filePath : PathLike ) :
+
+
+    def __init__ ( self, filePath: PathLike ) :
         self._filePath = filePath
 
+    def CountIfTagContains ( self, tag: str, needle: str ) :
+        return self._accumulateIfTagContains( tag, needle, _countResult() )
 
-    def CountIfTagContains ( self, tag : str, needle : str ) :
-        return self._accumulateIfTagContains( tag, needle, 0, lambda x, y : x + 1 )
+    def GetIfTagContains ( self, tag: str, needle: str ) :
+        return self._accumulateIfTagContains( tag, needle, _listResult() )
 
-    def GetIfTagContains ( self, tag : str, needle : str ) :
-        return self._accumulateIfTagContains( tag, needle, [], lambda x, y : x + [y] )
-
-
-    def _accumulateIfTagContains ( self, tag: str, needle: str, accumulator, action ) :
-        acc = accumulator
+    def _accumulateIfTagContains ( self, tag: str, needle: str, result : _searchResult ) :
         with FileReader( self._filePath ) as reader :
-            for decision in reader.IterateFromTo( '<Decision>', '</Decision>') :
+            for decision in reader.IterateFromTo( '<Decision>', '</Decision>' ) :
                 tagText = decision.GetTag( tag )
                 if needle in tagText :
-                    acc = action( acc, decision )
-        return acc
+                    result.Add( decision )
+        return result.Result
+
