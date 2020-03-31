ea76f9989043b87cf044f629d054c282c219decc
ps <scrv.pscrv@googlemail.com>
2020-04-01 00:03:32 +0200

    MatchMismatch

--- Xml/XmlAccess.py
+++ Xml/XmlAccess.py
@@ -6,24 +6,28 @@ from Xml.Predicates import SearchPredicate
 from Xml.XmlHandler import FileReader
 
 
-
 class _searchAccumulator( ABC ) :
 
-        @abstractmethod
-        def Add ( self, decision: Decision ) : ...
+    def __init__ ( self, predicate: SearchPredicate ) :
+        self._predicate = predicate
 
-        @property
-        @abstractmethod
-        def Result ( self ) : ...
+    @abstractmethod
+    def Add ( self, decision: Decision ) : ...
+
+    @property
+    @abstractmethod
+    def Result ( self ) : ...
 
 
 class _countAccumulator( _searchAccumulator ) :
 
-    def __init__ ( self ) :
+    def __init__ ( self, predicate: SearchPredicate ) :
+        super().__init__( predicate )
         self._result = 0
 
     def Add ( self, decision ) :
-        self._result += 1
+        if self._predicate.Evaluate( decision ) :
+            self._result += 1
 
     @property
     def Result ( self ) :
@@ -32,15 +36,54 @@ class _countAccumulator( _searchAccumulator ) :
 
 class _listAccumulator( _searchAccumulator ) :
 
-        def __init__ ( self ) :
-            self._result = [ ]
+    def __init__ ( self, predicate: SearchPredicate ) :
+        super().__init__( predicate )
+        self._result = [ ]
 
-        def Add ( self, decision: Decision ) :
+    def Add ( self, decision: Decision ) :
+        if self._predicate.Evaluate( decision ) :
             self._result.append( decision )
 
-        @property
-        def Result ( self ) :
-            return self._result
+    @property
+    def Result ( self ) :
+        return self._result
+
+
+class _matchAndMismatchCountAccumulator( _searchAccumulator ) :
+
+    def __init__ ( self, predicate: SearchPredicate ) :
+        super().__init__( predicate )
+        self._matchResult = 0
+        self._mismatchResult = 0
+
+    def Add ( self, decision: Decision ) :
+        if self._predicate.Evaluate( decision ) :
+            self._matchResult += 1
+        else :
+            self._mismatchResult += 1
+
+    @property
+    def Result ( self ) :
+        return { 'Match' : self._matchResult, 'Mismatch' : self._mismatchResult }
+
+
+class _matchAndMismatchListAccumulator( _searchAccumulator ) :
+
+    def __init__ ( self, predicate: SearchPredicate ) :
+        super().__init__( predicate )
+        self._matchResult = [ ]
+        self._mismatchResult = [ ]
+
+    def Add ( self, decision: Decision ) :
+        if self._predicate.Evaluate( decision ) :
+            self._matchResult.append( decision )
+        else :
+            self._mismatchResult.append( decision )
+
+    @property
+    def Result ( self ) :
+        return { 'Match' : self._matchResult, 'Mismatch' : self._mismatchResult }
+
 
 
 class Accessor :
@@ -48,19 +91,20 @@ class Accessor :
     def __init__ ( self, filePath: PathLike ) :
         self._filePath = filePath
 
+    def CountMatches ( self, predicate: SearchPredicate ) :
+        return self._search( _countAccumulator( predicate ) )
 
-    def CountSearch ( self, predicate : SearchPredicate ) :
-        return self._predicateSearch( predicate, _countAccumulator() )
+    def CountMatchesAndMismatches ( self, predicate : SearchPredicate ) :
+        return self._search( _matchAndMismatchCountAccumulator( predicate ))
 
-    def ListSearch ( self, predicate : SearchPredicate ) :
-        return self._predicateSearch( predicate, _listAccumulator() )
+    def ListMatches ( self, predicate: SearchPredicate ) :
+        return self._search( _listAccumulator( predicate ) )
 
-    def _predicateSearch ( self, _predicate : SearchPredicate, result : _searchAccumulator ) :
+    def ListMatchesAndMismatches ( self, predicate : SearchPredicate ) :
+        return self._search( _matchAndMismatchListAccumulator( predicate ) )
+
+    def _search ( self, result: _searchAccumulator ) :
         with FileReader( self._filePath ) as reader :
             for decision in reader.IterateFromTo( '<Decision>', '</Decision>' ) :
-                if _predicate.Evaluate ( decision ) :
-                    result.Add( decision )
+                result.Add( decision )
             return result.Result
-
-
-
